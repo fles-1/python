4c8f20b3c1ebe995ae70efceb8d6a57070453133
fles-1 <54968393+fles-1@users.noreply.github.com>
2019-12-10 11:23:52 +0800

    Update time_study.py

--- time_study.py
+++ time_study.py
@@ -1,5 +1,9 @@
 import time
+import datetime
 #获取当前时间戳
 t=time.time()
 local_time=time.localtime(t)#获取本地时间
 now=time.strftime('%Y-%m-%d %H:%m:%S',local_time)#获取本地格式化的时间
+#获取前第3天日期
+d3=(datetime.datetime.now() + timedelta(days=-3)).strftime('%Y-%m-%d')
+print(d3)
